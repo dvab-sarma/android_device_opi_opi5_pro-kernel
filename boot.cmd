6e043c6a238941b18a008c51d7a74bdc96211c54
dvab-sarma <vdarbha0473@gmail.com>
2025-12-10 13:47:58 -0600

    Adding NVME and USB boot functionalites.

--- boot.cmd
+++ boot.cmd
@@ -1,25 +1,31 @@
 setenv load_addr "0x9000000"
-echo ${devtype}
-echo ${devnum}
-echo ${prefix}
+setenv boot_device "mmc"
+setenv boot_devnum "0"
+setenv boot_partnum "1"
+setenv root_partnum "2"
+setenv prefix "/"
+
 load ${devtype} ${devnum} ${load_addr} ${prefix}config.txt
 env import -t ${load_addr} ${filesize}
 
-load mmc 0:1 ${kernel_addr_r} /Image
-load mmc 0:1 ${fdt_addr_r} /${fdtfile}
+load ${boot_device} ${boot_devnum}:${boot_partnum} ${kernel_addr_r} ${prefix}Image
+load ${boot_device} ${boot_devnum}:${boot_partnum} ${fdt_addr_r} ${prefix}${fdtfile}
 
 if test "${recovery}" = "true"; then
     echo "Booting into Recovery...."
-    load mmc 0:1 ${fdtoverlay_addr_r} /${fdtoverlay}
+    load ${boot_device} ${boot_devnum}:${boot_partnum} ${fdtoverlay_addr_r} /${fdtoverlay}
     fdt addr ${fdt_addr_r}
     fdt resize
     fdt apply ${fdtoverlay_addr_r}
-    load mmc 0:1 ${ramdisk_addr_r} /uRecovery
+    load ${boot_device} ${boot_devnum}:${boot_partnum} ${ramdisk_addr_r} /uRecovery
 else
     echo "Booting into normal Android...."
-    load mmc 0:1 ${ramdisk_addr_r} /uRamdisk
+    load ${boot_device} ${boot_devnum}:${boot_partnum} ${ramdisk_addr_r} /uRamdisk
 fi;
 
 
-setenv bootargs "loglevel=8 earlycon=uart8250,mmio32,0xfeb50000 console=ttyS2,1500000 root=/dev/ram0 rootwait androidboot.hardware=opi5 androidboot.selinux=permissive"
+part uuid ${boot_device} ${boot_devnum}:${root_partnum} partuuid
+
+setenv bootargs "loglevel=8 earlycon=uart8250,mmio32,0xfeb50000 console=ttyS2,1500000 root=/dev/ram0 rootwait pcie_aspm=off pcie_port_pm=off nvme_core.default_ps_max_latency_us=0 androidboot.boot_part_uuid=${partuuid} androidboot.hardware=opi5 androidboot.selinux=permissive"
+
 booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
