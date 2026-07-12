947610cba28b52b0b4b3da3f94e1452827ebbb36
Venkata Atchuta Bheemeswara Sarma Darbha <vdarbha0473@gmail.com>
2026-07-12 03:16:07 -0500

    Android: Device: Kernel: add working TWRP recovery and kernel image which fixes the rkvdec hevc null pointer kernel panic.
    Now, we dont need devicetree to set the partition layout, the boot.cmd detects the type of device and automatically assigns
    which labels belong to which partition.

--- boot.cmd
+++ boot.cmd
@@ -13,19 +13,18 @@ load ${boot_device} ${boot_devnum}:${boot_partnum} ${fdt_addr_r} ${prefix}${fdtf
 
 if test "${recovery}" = "true"; then
     echo "Booting into Recovery...."
-    load ${boot_device} ${boot_devnum}:${boot_partnum} ${fdtoverlay_addr_r} /${fdtoverlay}
-    fdt addr ${fdt_addr_r}
-    fdt resize
-    fdt apply ${fdtoverlay_addr_r}
     load ${boot_device} ${boot_devnum}:${boot_partnum} ${ramdisk_addr_r} /uRecovery
+    setenv recovery_bootargs "androidboot.boot_device=${boot_device}"
 else
     echo "Booting into normal Android...."
     load ${boot_device} ${boot_devnum}:${boot_partnum} ${ramdisk_addr_r} /uRamdisk
+    setenv recovery_bootargs ""
 fi;
 
 
 part uuid ${boot_device} ${boot_devnum}:${root_partnum} partuuid
 
-setenv bootargs "loglevel=8 earlycon=uart8250,mmio32,0xfeb50000 console=ttyS2,1500000 root=/dev/ram0 rootwait pcie_aspm=off pcie_port_pm=off nvme_core.default_ps_max_latency_us=0 androidboot.boot_part_uuid=${partuuid} androidboot.hardware=opi5 androidboot.selinux=permissive"
+setenv bootargs "loglevel=8 earlycon=uart8250,mmio32,0xfeb50000 console=ttyS2,1500000 root=/dev/ram0 rootwait pcie_aspm=off pcie_port_pm=off nvme_core.default_ps_max_latency_us=0 androidboot.boot_part_uuid=${partuuid} ${recovery_bootargs} androidboot.hardware=opi5 androidboot.selinux=permissive"
 
 booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
+
